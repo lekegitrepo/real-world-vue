b109b60c61c9ad2c6c7fc43b2ea3dbbf6b5c567e
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-15 16:17:52 +0100

    add event show link

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,5 +1,10 @@
 <template>
-  <h1>Events Listing</h1>
+  <section>
+    <h1>Events Listing</h1>
+    <router-link :to="{ name: 'event', params: { id: '1' } }"
+      >Show Event</router-link
+    >
+  </section>
 </template>
 
 <script>
