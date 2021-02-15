63a57b51fdb6614624d6adbf718ab53944e07d7c
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-15 18:02:35 +0100

    add EventCard content

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,7 +1,47 @@
-<template> </template>
+<template>
+  <div class="event-card -shadow">
+    <span class="eyebrow">@{{ event.time }} on {{ event.date }}</span>
+    <h4 class="title">{{ event.title }}</h4>
+    <span>{{ event.attendees.length }} attending</span>
+  </div>
+</template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      event: {
+        title: 'Learning Vue JS',
+        id: 1,
+        date: 'Wed Oct 10, 2021',
+        time: '3:30',
+        attendees: [
+          { id: 'xae2y', name: 'Steffen' },
+          { id: 'li98j', name: 'Alex' }
+        ]
+      }
+    }
+  }
+}
 </script>
 
-<style></style>
+<style scoped>
+.event-card {
+  padding: 20px;
+  margin-bottom: 24px;
+  transition: all 0.2s linear;
+  cursor: pointer;
+}
+.event-card:hover {
+  transform: scale(1.01);
+  box-shadow: 0 3px 12px 0 rgba(0, 0, 0, 0.2), 0 1px 15px 0 rgba(0, 0, 0, 0.19);
+}
+.event-card > .title {
+  margin: 0;
+}
+.event-link {
+  color: black;
+  text-decoration: none;
+  font-weight: 100;
+}
+</style>
