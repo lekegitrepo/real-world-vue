4d7a92dabb60e00f1f0a5cebdd156b22a8f5e219
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-15 18:06:18 +0100

    add event show link to the card

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,9 +2,6 @@
   <section>
     <h1>Events Listing</h1>
     <EventCard />
-    <router-link :to="{ name: 'event', params: { id: '1' } }"
-      >Show Event</router-link
-    >
   </section>
 </template>
 
