0abad195c58ab044a6f630314799b34ae4ddbafe
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-15 20:54:55 +0100

    style the event show page

--- src/views/EventShow.vue
+++ src/views/EventShow.vue
@@ -54,4 +54,23 @@ export default {
 }
 </script>
 
-<style></style>
+<style scope>
+.location {
+  margin-bottom: 0;
+}
+.location > .icon {
+  margin-left: 10px;
+}
+.event-header > .title {
+  margin: 0;
+}
+.list-group {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+.list-group > .list-item {
+  padding: 1em 0;
+  border-bottom: solid 1px #e5e5e5;
+}
+</style>
