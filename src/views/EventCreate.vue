2513152e009af6f20df41e5e6ec5d4f94ad9e5ba
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-16 16:44:15 +0100

    create createFreshEventObject function

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -60,8 +60,41 @@ import Datepicker from 'vuejs-datepicker'
 export default {
   components: {
     Datepicker
+  },
+  data() {
+    const times = []
+    for (let i = 1; i <= 24; i++) {
+      times.push(i + ':00')
+    }
+    return {
+      times,
+      categories: this.$store.state.categories,
+      event: this.createFreshEventObject()
+    }
+  },
+  methods: {
+    createFreshEventObject() {
+      const user = this.$store.state.user
+      const id = Math.floor(Math.random() * 10000000)
+      return {
+        id: id,
+        user: user,
+        category: '',
+        organizer: user,
+        title: '',
+        description: '',
+        location: '',
+        date: '',
+        time: '',
+        attendees: []
+      }
+    }
   }
 }
 </script>
 
-<style></style>
+<style scope>
+.field {
+  margin-bottom: 24px;
+}
+</style>
