154d88e794320c20d37186066a92234fc46c67f7
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-16 20:21:34 +0100

    create notification state module

---
+++ src/store/modules/notification.js
@@ -0,0 +1,21 @@
+export const namespaced = true
+
+export const state = {
+  notifications: []
+}
+
+let nextId = 1
+
+export const mutations = {
+  PUSH(state, notification) {
+    state.notifications.push({
+      ...notification,
+      id: nextId++
+    })
+  },
+  DELETE(state, notificationToRemove) {
+    state.notifications = state.notifications.filter(
+      notification => notification.id !== notificationToRemove.id
+    )
+  }
+}
