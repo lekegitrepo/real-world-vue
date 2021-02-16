58280d5966101baad25367e889fd1c9d0b252c42
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-16 19:35:28 +0100

    implement state namespaced

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -30,7 +30,7 @@ export default {
   },
   created() {
     this.perPage = 3
-    this.$store.dispatch('fetchEvents', {
+    this.$store.dispatch('event/fetchEvents', {
       perPage: this.perPage,
       page: this.page
     })
