e8ebc81d42b56865bb6911cb7ac9e3a19c854085
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-21 17:08:11 +0100

    create Network issue component view

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h4>There is a network issue please try again later</h4>
+    <router-link :to="{ name: 'events' }">Return to the Home page</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
