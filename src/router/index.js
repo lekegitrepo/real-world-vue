47164f6c6e416bd6baea9fa511ef50e8dba92937
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-02-21 17:14:24 +0100

    add network issue to the route

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import EventList from '../views/EventList.vue'
 import EventShow from '../views/EventShow.vue'
 import EventCreate from '../views/EventCreate.vue'
 import NotFound from '../views/NotFound.vue'
+import NetworkIssue from '../views/NetworkIssue.vue'
 import NProgress from 'nprogress'
 import store from '@/store'
 
@@ -28,7 +29,13 @@ const routes = [
           routeTo.params.event = event
           next()
         })
-        .catch(() => next({ name: '404', params: { resource: 'event' } }))
+        .catch(error => {
+          if (error.response && error.response.status == 404) {
+            next({ name: '404', params: { resource: 'event' } })
+          } else {
+            next({ name: 'network-issue' })
+          }
+        })
     }
   },
   {
@@ -41,6 +48,11 @@ const routes = [
     name: '404',
     component: NotFound,
     props: true
+  },
+  {
+    path: '/network-issue',
+    name: 'network-issue',
+    component: NetworkIssue
   },
   {
     path: '*',
