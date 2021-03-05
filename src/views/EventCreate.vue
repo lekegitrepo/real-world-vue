aedf8e0e711b2f1716498be4496d7f71e5e7c69c
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-03-05 16:12:17 +0100

    update the input elements in the EventCreate view

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -16,24 +16,22 @@
         class="field"
       />
 
-      <div class="field">
-        <label>Description</label>
-        <input
-          v-model="event.description"
-          type="text"
-          placeholder="Add a description"
-        />
-      </div>
+      <BaseInput
+        type="text"
+        placeholder="Add a Description"
+        label="Description"
+        v-model="event.description"
+        class="field"
+      />
 
       <h3>Where is your event?</h3>
-      <div class="field">
-        <label>Location</label>
-        <input
-          v-model="event.location"
-          type="text"
-          placeholder="Add a location"
-        />
-      </div>
+      <BaseInput
+        type="text"
+        placeholder="Location"
+        label="Location"
+        v-model="event.location"
+        class="field"
+      />
 
       <h3>When is your event?</h3>
 
