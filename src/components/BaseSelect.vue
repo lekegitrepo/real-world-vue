f3ddbe5e042efd92c2af052022ed04840711b688
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-03-07 16:29:26 +0100

    create BaseSelect component

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <select :value="value" @input="updateValue" v-bind="$attrs">
+      <option v-for="cat in categories" :key="cat">{{ cat }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    value: [String, Number]
+  },
+  methods: {
+    updateValue(event) {
+      this.$emit('input', event.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
