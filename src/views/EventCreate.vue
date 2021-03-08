3a5ede3415ef65a78184951df1e1ee0edeb01abe
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-03-08 07:51:25 +0100

    add BaseButton to the EventCreate view

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -49,7 +49,8 @@
         class="field"
       />
 
-      <input type="submit" class="button -fill-gradient" value="Submit" />
+      <!-- <input type="submit" class="button -fill-gradient" value="Submit" /> -->
+      <BaseButton type="submit">Submit</BaseButton>
     </form>
   </div>
 </template>
