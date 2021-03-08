eac03005788225de57e21f5380083b9c62f1d68e
Temitayo Adeleke <lekedevrepo@gmail.com>
2021-03-08 07:56:35 +0100

    create buttonClass prop

--- src/components/BaseButton.vue
+++ src/components/BaseButton.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <button v-on="$listeners" class="button">
+    <button v-on="$listeners" class="button" :class="buttonClass">
       <slot />
     </button>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    buttonClass: {
+      type: String
+    }
+  }
+}
 </script>
 
 <style scoped></style>
